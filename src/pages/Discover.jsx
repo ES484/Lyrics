2b0dc3b99609a68fbd4046943f0aa70fe74039e4
ES484 <esraasaad478@gmail.com>
2022-11-20 23:35:19 +0200

    select edit

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -6,19 +6,17 @@ import { selectGenreListId } from '../redux/features/playerSlice';
 const Discover = () => {
     const dispatch = useDispatch();
     const {activeSong, isPlaying, genreListId} = useSelector((state)=>state.player);
-    const {data, isFetching, error} = useGetSongsByGenreQuery(`${genreListId.toUpperCase()}` || 'POP');
+    const {data, isFetching, error} = useGetSongsByGenreQuery(genreListId || 'POP');
+    console.log(genreListId)
     
-    const onChangeHandler = (e) => {
-        dispatch(selectGenreListId(e.target.value));
-    }
     if(isFetching){return<Loader title="Loading..." />}
     if(error){return<Error/>}
     return (
         <div className='flex flex-col'>
             <div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
                 <h2 className='capitalize text-bold text-white text-3xl'>discover</h2>
-                <select className='text-gray-300 bg-black rounded-lg p-1' onChange={(e)=>{onChangeHandler(e)}}
-                value={genreListId||'Film'}>
+                <select className='text-gray-300 bg-black rounded-lg p-1' onChange={(e)=>dispatch(selectGenreListId(e.target.value.toUpperCase()))}
+                value={genreListId ||'pop'}>
                     {genres.map((genera)=><option  key={genera.value}>{genera.title}</option>)}
                 </select>
             </div>
